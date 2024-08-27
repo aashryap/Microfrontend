0e14f3051240c05bf4dbe6267e784dff7ff449c8
Aashrya Pandya <aashryap@gmail.com>
2024-08-27 10:34:47 +0530

    updated app.js to see deployment changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp"
 
 export default () => {
     return <>
-     <h1>CONTAINER</h1>
+     <h1>CONTAINER INIT</h1>
      <hr />
      <MarketingApp />
     </>
